18bb1667754e083d55c9dc4a5be74448bc88e0b7
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-20 22:52:06 -0300

    Rename and improve component style

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Panel = styled.div`
   background-color: #ffffff;
-  border-radius: 4px;
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-  margin: 80px auto;
-  max-width: 700px;
   padding: 30px;
+  width: 100%;
 
   h1 {
     align-items: center;
+    color: #7B7A7A;
     display: flex;
     flex-direction: row;
     font-size: 20px;
@@ -20,4 +19,4 @@ const Container = styled.div`
   }
 `;
 
-export default Container;
+export default Panel;
