de40cbec12009b40f5154d63473b327649f1b5e5
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-02-08 18:48:31 -0300

    Move tests to root

--- __tests__/pages/Main.test.js
+++ __tests__/pages/Main.test.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react';
 import { BrowserRouter, Router } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { createBrowserHistory } from 'history';
 import MockAdapter from 'axios-mock-adapter';
+import faker from 'faker';
 
+import factory from '../utils/factories';
 import api from '~/services/api';
 import Main from '~/components/pages/Main';
 
 const history = createBrowserHistory();
 const api_mock = new MockAdapter(api);
+const login = faker.internet.userName();
+const name = faker.lorem.word();
+
+let repository;
 
 describe('Main page', () => {
+  beforeAll(async () => {
+    repository = await factory.attrs('Repository', {
+      name,
+      full_name: `${login}/${name}`,
+      owner: {
+        login,
+        avatar_url: faker.image.imageUrl(),
+      },
+    });
+
+    api_mock.onGet(`repos/${name}`).reply(200, repository);
+  });
+
   beforeEach(async () => {
     await act(async () => {
       localStorage.clear();
@@ -18,7 +38,7 @@ describe('Main page', () => {
   });
 
   it('should be able to go to repository details', () => {
-    localStorage.setItem('repositories', JSON.stringify([{ name: 'test' }]));
+    localStorage.setItem('repositories', JSON.stringify([{ name }]));
 
     const { getByTestId } = render(
       <Router history={history}>
@@ -26,14 +46,13 @@ describe('Main page', () => {
       </Router>
     );
 
-    fireEvent.click(getByTestId('repository_test'));
-    expect(history.location.pathname).toBe('/repository/test');
+    fireEvent.click(getByTestId(`repository_${name}`));
+    expect(history.location.pathname).toBe(`/repository/${name}`);
   });
 
   it('should be able to add a repository', async () => {
-    api_mock
-      .onGet('https://api.github.com/repos/test')
-      .reply(200, { full_name: 'test' });
+    api_mock.onGet(`repos/${name}`).reply(200, repository);
+
     const { getByPlaceholderText, getByTestId } = render(
       <BrowserRouter>
         <Main />
@@ -42,7 +61,7 @@ describe('Main page', () => {
     const input = getByPlaceholderText('username/project');
 
     await act(async () => {
-      fireEvent.change(input, { target: { value: 'test' } });
+      fireEvent.change(input, { target: { value: name } });
     });
 
     await act(async () => {
@@ -51,12 +70,12 @@ describe('Main page', () => {
 
     expect(input.value).toBe('');
     expect(localStorage.__STORE__.repositories).toBe(
-      JSON.stringify([{ name: 'test' }])
+      JSON.stringify([{ name: `${login}/${name}` }])
     );
   });
 
   it('should be not able to add a repository', async () => {
-    localStorage.setItem('repositories', JSON.stringify([{ name: 'test' }]));
+    localStorage.setItem('repositories', JSON.stringify([{ name }]));
 
     const { getByPlaceholderText, getByTestId } = render(
       <BrowserRouter>
@@ -66,14 +85,14 @@ describe('Main page', () => {
     const input = getByPlaceholderText('username/project');
 
     await act(async () => {
-      fireEvent.change(input, { target: { value: 'test' } });
+      fireEvent.change(input, { target: { value: name } });
     });
 
     await act(async () => {
       fireEvent.submit(getByTestId('submit'));
     });
 
-    expect(input.value).toBe('test');
+    expect(input.value).toBe(name);
     expect(getByTestId('error')).toHaveTextContent('Repositório duplicado');
   });
 });
