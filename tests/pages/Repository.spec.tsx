52668697497990f7e3c0a8d807050a8abf0957c9
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-15 09:10:00 -0300

    Update tests

--- tests/pages/Repository.spec.tsx
+++ tests/pages/Repository.spec.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import MockAdapter from 'axios-mock-adapter';
-import { render, act } from '@testing-library/react';
-import { MemoryRouter, Route } from 'react-router-dom';
+import {
+  render,
+  act,
+  waitFor,
+  fireEvent,
+  Matcher,
+  SelectorMatcherOptions,
+} from '@testing-library/react';
+import { Router, Route, Routes } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 import factory from '../utils/factory';
 import api from '../../src/services/api';
@@ -23,7 +31,7 @@ interface Issue {
 describe('Repository page', () => {
   const apiMock = new MockAdapter(api);
 
-  it('should exists a link to go back to dashboard page', async () => {
+  it('should be able to go back to dashboard page', async () => {
     const repository: GitHubRepository = await factory.attrs('Repository');
     const issues: Issue[] = await factory.attrsMany('Issue', 3);
 
@@ -33,20 +41,30 @@ describe('Repository page', () => {
       .onGet(`/repos/${repository.full_name}/issues`)
       .reply(200, issues);
 
-    await act(async () => {
-      const { getByText } = render(
-        <MemoryRouter
-          initialEntries={[`/repositories/${repository.full_name}`]}
-        >
-          <Route path="/repositories/:repository+">
-            <Repository />
-          </Route>
-          ,
-        </MemoryRouter>,
-      );
+    const history = createMemoryHistory();
+    history.push(`/repositories/${repository.full_name}`);
 
-      expect(getByText('Voltar')).toBeInTheDocument();
-      expect(getByText('Voltar')).toHaveAttribute('href', '/');
+    let getByText: (
+      id: Matcher,
+      options?: SelectorMatcherOptions | undefined,
+    ) => HTMLElement;
+
+    await act(async () => {
+      const component = render(
+        <Router location={history.location} navigator={history}>
+          <Routes>
+            <Route path="/repositories/*" element={<Repository />} />
+          </Routes>
+        </Router>,
+      );
+      getByText = component.getByText;
     });
+
+    await waitFor(() => getByText('Voltar'));
+
+    await act(async () => {
+      fireEvent.click(getByText('Voltar'));
+    });
+    expect(history.location.pathname).toBe('/');
   });
 });
